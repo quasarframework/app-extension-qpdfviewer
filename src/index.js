1bb9aa18f03e900ee06adf316b6d1ef903c987c7
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-04-15 16:44:16 -0600

    fix: Now runs with SSR

--- src/index.js
+++ src/index.js
@@ -10,19 +10,22 @@ const extendConf = function (api, conf) {
   let boot = conf.boot
 
   // make sure qpdfviewer boot file is registered
-  if (!boot.includes('~@quasar/quasar-app-extension-qpdfviewer/src/boot/qpdfviewer.js')) {
-    boot.push('~@quasar/quasar-app-extension-qpdfviewer/src/boot/qpdfviewer.js')
-    // make sure boot file transpiles
-    conf.build.transpileDependencies.push(/quasar-app-extension-qpdfviewer[\\/]src[\\/]boot/)
+  const bootFile = '~@quasar/quasar-app-extension-qpdfviewer/src/boot/qpdfviewer.js'
+  if (!boot.includes(bootFile)) {
+    boot.push(bootFile)
     console.log(` App Extension (qpdfviewer) Info: 'Adding qpdfviewer boot reference to your quasar.conf.js'`)
   }
+
+  // make sure boot file transpiles
+  conf.build.transpileDependencies.push(/quasar-app-extension-qpdfviewer[\\/]src/)
 
   // for brevity
   let css = conf.css
 
   // make sure qpdfviewer css goes through webpack to avoid ssr issues
-  if (!css.includes('~@quasar/quasar-app-extension-qpdfviewer/src/component/pdfviewer.styl')) {
-    css.push('~@quasar/quasar-app-extension-qpdfviewer/src/component/pdfviewer.styl')
+  const cssFile = '~@quasar/quasar-app-extension-qpdfviewer/src/component/pdfviewer.styl'
+  if (!css.includes(cssFile)) {
+    css.push(cssFile)
     console.log(` App Extension (qpdfviewer) Info: 'Adding pdfviewer.styl css reference to your quasar.conf.js'`)
   }
 }
