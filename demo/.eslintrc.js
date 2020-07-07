4ef0f3606ec92ceeb26b417a19f53e4e9d6945b4
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-07 10:56:26 -0600

    chore: adjustment to eslint params

--- demo/.eslintrc.js
+++ demo/.eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
 
     'no-void': 'off',
     // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
