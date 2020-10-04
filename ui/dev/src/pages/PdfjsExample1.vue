aff1a6567047049ea52327ca164d3a7c29c6961a
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-10-04 11:07:50 -0600

    chore: add examples to dev

---
+++ ui/dev/src/pages/PdfjsExample1.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="flex flex-center page-static-height">
+    <q-pdfviewer
+      :src="updatedSrc"
+      content-class="absolute"
+    />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'Page1',
+
+  data () {
+    return {
+      src: 'pdf/pdf-test.pdf'
+    }
+  },
+
+  methods: {
+    // do some funky stuff because this site is
+    // using history mode with publicPath, and it makes
+    // pdfjs more comfortable using full urls
+    getLocation (source) {
+      let url = location.href
+      if (location.href[location.href.length - 1] !== '/') {
+        url += '/'
+      }
+      url += '../' + source
+      return url
+    }
+  },
+  computed: {
+    updatedSrc () {
+      if (process.env.MODE === 'electron') {
+        return '/' + this.src
+      }
+      return this.getLocation(this.src)
+    }
+  }
+}
+</script>
