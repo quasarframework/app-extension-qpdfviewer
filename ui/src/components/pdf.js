ffc9ad0723dd54b5a67606c17a3140b4c3086422
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-10-04 08:50:51 -0600

    chore: remove debugger statement

--- ui/src/components/pdf.js
+++ ui/src/components/pdf.js
@@ -4,8 +4,6 @@
 
 const PdfjsWorker = require("worker-loader?esModule=false!pdfjs-dist/build/pdf.worker.js")
 const PDFJS = require('pdfjs-dist/build/pdf.js')
-debugger
-console.log(PdfjsWorker)
 PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker()
 // PDFJS.GlobalWorkerOptions.workerSrc = PdfjsWorker
 
